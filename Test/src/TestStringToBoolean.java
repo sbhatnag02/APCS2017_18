f8ffd0ae4016790151c3cef2930f31245ec81895
409351 <409351@10.72.6.14>
2017-11-22 08:07:41 -0500

    Created a Java Project for testing methods/pieces of code

---
+++ Test/src/TestStringToBoolean.java
@@ -0,0 +1,8 @@
+
+public class TestStringToBoolean {
+	public static void main(String[] args) {
+		String expression = "2>1";
+		int i = 0;
+		System.out.println(Boolean.valueOf(expression));
+	}
+}
