3f4c75ea9576ffd781dbc9515225cce268496fd8
409351 <409351@10.72.6.20>
2018-01-09 10:01:07 -0500

    Finished ps08

--- APCS Problem Set 08/src/net/mrpaul/apcs/pb050/ps08/ArraysPotpourri.java
+++ APCS Problem Set 08/src/net/mrpaul/apcs/pb050/ps08/ArraysPotpourri.java
@@ -1,5 +1,12 @@
 package net.mrpaul.apcs.pb050.ps08;
 
+import java.util.Arrays;
+import java.util.Collections;
+/**
+ * ArraysPotpourri.java -- Different exercises that manipulate arrays in different ways
+ * @author Sarthak Bhatnagar
+ *
+ */
 public class ArraysPotpourri {
 	/**
 	 * Finds the last occurrence of a number in an array. If the number isn't found, it returns -1
@@ -12,8 +19,8 @@ public class ArraysPotpourri {
 		int index = -1;
 		
 		for(int i = 0; i < array.length; i++) {
-			if(array[i] == desiredElement) {
-				index = i;
+			if(array[i] == desiredElement) { //Check for desired element
+				index = i; //if true, save the current position in local variabel
 			}
 		}
 		
@@ -28,19 +35,22 @@ public class ArraysPotpourri {
 	public static int range(int[] array) {
 		int smallestNumber = array[0];
 		for(int i = 1; i < array.length; i++) {
+			//Find the smallest number
 			if(array[i] < smallestNumber) {
 				smallestNumber = array[i];
 			}
 		}
 		int largestNumber = array[0];
+		//find the largest number
 		for(int i = 1; i < array.length; i++) {
 			if(array[i] > largestNumber) {
 				largestNumber = array[i];
 			}
 		}
+		//If the array has only one element, return 1
 		if(array.length == 1) {
 			return 1;
-		}else {
+		}else { //Otherwise find the difference of the largest and smallest number
 			return (largestNumber - smallestNumber) + 1;
 		}
 	}
@@ -55,10 +65,13 @@ public class ArraysPotpourri {
 	public static int countInRange(int[] array, int min, int max) {
 		int count = 0;
 		for(int i = 0; i < array.length; i++) {
+			//Check if element is in range
 			if(array[i] >= min && array[i] <= max) {
+				//If in range, increase counter by one
 				count++;
 			}
 		}
+		//return counter
 		return count;
 	}
 	/**
@@ -68,11 +81,13 @@ public class ArraysPotpourri {
 	 * @return true if it is sorted, false if it is not
 	 */
 	public static boolean isSorted(int[] array) {
+		//If array length is one, return true, otherwise check to see if array is sorted
 		if(array.length == 1) {
 			return true;
 		}else {
 			int prev = array[0];
 			boolean sorted = true;
+			//Check if array is sorted in ascending order
 			for(int i = 1; i < array.length; i++) {
 				if(array[i] < prev) {
 					sorted = false;
@@ -80,6 +95,7 @@ public class ArraysPotpourri {
 					prev = array[i];
 				}
 			}
+			//return true or false, depending on whether or not it is sorted
 			if(!sorted) {
 				return false;
 			}else {
@@ -88,30 +104,302 @@ public class ArraysPotpourri {
 		}
 	
 	}
-	
+	/**
+	 * Find mode of a set of elements, passed in by an array
+	 * <p>
+	 * @param array
+	 * @return the mode of the data set (array)
+	 */
+	public static int mode(int[] array) {
+	    int mode = array[0];
+	    int maxCount = 0;
+	    //Iterate through each element of the array
+	    for (int i = 0; i < array.length; i++) {
+	        int currentElement = array[i];
+	        int count = 1;
+	        //check to see how many times element occurs, and whether it occurs the most
+	        for (int j = 0; j < array.length; j++) {
+	            if (array[j] == currentElement) {
+	            		count++;
+	            	}
+	            //if it occurs the most, save the number of occurrences and the number itself
+	            if (count > maxCount) {
+	                mode = currentElement;
+	                maxCount = count;
+	            }
+	        }
+	    }
+	    //return mode
+	    return mode;
+	}
+	/**
+	 * Calculates the standard deviation of all elements in an integer array
+	 * <p>
+	 * @param array
+	 * @return standard deviation
+	 */
 	public static double stdev(int[] array) {
 		int sum = 0;
+		//get sum
 		for (int i = 0; i < array.length; i++) {
 			sum += array[i];
 		}
+		//get average
 		double average = sum/array.length;
 		double sumDif = 0;
+		//Calculate standard deviation
 		for (int i = 0; i < array.length; i++) {
 			sumDif += Math.pow((array[i] - average), 2);
 		}
+		//return square root of the sum of differences
 		return Math.sqrt(sumDif/(array.length-1));
 	}
-	
+	/**
+	 * Find the kth largest element in the array
+	 * <p>
+	 * @param array
+	 * @param k
+	 * @return
+	 */
+	public static int kthLargest(int[] array, int k) {
+		//Sort the array in numerical order
+		Arrays.sort(array);
+		//Return the desired element
+		return array[array.length - 1 - k];
+	}
+	/**
+	 * Determines the minimum 'gap' between adjacent values in the array. The gap is the difference between the adjacent elements
+	 * <p>
+	 * @param array
+	 * @return the minGap, in the form of an integer. If the array length is less than 2, it will return 0	
+	 */
+	public static int minGap(int [] array) {
+		if(!(array.length < 2)) { //Check if array has more than one element
+			int difference = 0;
+			int[] differences = new int[array.length-1]; 
+			for(int i = 0; i < array.length-1; i++) { //Store differences of elements in an array
+				differences[i] = array[i+1] - array[i];
+			}
+			int minGap = differences[0];
+			for(int i = 0; i < differences.length; i++) { //look through array for smallest difference
+				if(differences[i] < minGap) {
+					minGap = differences[i];
+				}
+			}
+			//return smallest difference
+			return minGap;
+		}else {
+			return 0;
+		}
+	}
+	/**
+	 * Determines the percent of the elements in an array that are even
+	 * <p>
+	 * @param array
+	 * @return the percent of even elements in the array as a double
+	 */
+	public static double percentEven(int[] array) {
+		int numEven = 0;
+		int totalElements = array.length;
+		if(totalElements > 0) { //If array isn't empty
+			for(int i = 0; i < array.length; i++) {
+				if(array[i] % 2 == 0) {
+					numEven++; //Increase counter for each even number
+				}
+			}
+			double percent = ((double)numEven/totalElements) * 100; //Find percent of even numbers and return the value
+			return percent;
+		}else {
+			return 0.0;
+		}
+	}	
+	/**
+	 * Determines whether an array is unique or not. A unique array is an array in which no values repeat. It will return true
+	 * if the array is unique, or false if it isn't unique
+	 * <p>
+	 * @param array
+	 * @return true if unique, false if not unique
+	 */
 	public static boolean isUnique(int[] array) {
 		boolean unique = true;
-		for (int i = 0; i < array.length; i++) {
-		  for (int j = i+ 1; j < array.length; j++) {
+		for (int i = 0; i < array.length; i++) {//Iterate through each element
+		  for (int j = i+ 1; j < array.length; j++) {//check if the current element repeats in the array again
 		    if (j != i && array[j] == array[i]) {
-		    	unique = false;
+		    		unique = false;
 		    }
 		  }
 		}
 		return unique;
 	}
-	
+	/**
+	 * Based on a user input, the method will find the element in the array that is closest to the input, but also smaller than the input
+	 * <p>
+	 * @param array -- Numbers to look through and compare to user input
+	 * @param price -- User guess
+	 */
+	public static int priceIsRight(int[] array, int price){
+        int closestNum = -1;
+        int biggestNumber = 0;
+        for (int i = 0; i < array.length; i++) {
+        		if (array[i] > biggestNumber) {
+        			biggestNumber = array[i];
+        		}
+        }
+        //Set smallestDif to the biggest number so it is guaranteed to get replaced. 
+        int smallestDiff = biggestNumber + 1;
+        for (int i = 0; i < array.length; i++){
+            int currentElement = array[i];
+            if (currentElement <= price){  //Check to see if current element is greater than guess
+                int diff = price - currentElement; //Find the difference
+                if (diff < smallestDiff){ //If the calculated difference is smaller than the smallest difference, set the current element to the best bid
+                    smallestDiff = diff;
+                    closestNum = currentElement;
+                }
+            }
+        }
+        return closestNum;
+    }
+	/**
+	 * This method finds the longest consecutive subsequence inside an array, and returns that value
+	 * <p>
+	 * @param array
+	 * @return The longest consecutive sequence
+	 */
+	public static int longestSortedSequence(int[] array) {
+		//Initialize counters
+		int[] longestCounts = new int[array.length];
+		int currentLongestCount = 0;
+		int longestCountIndex = 0;
+		//Iterate through each element in the array
+		for(int i = 0; i < array.length; i++) {
+			if(i < array.length-1 && array[i] <= array[i+1]) {//If it's ascending, increase counter
+				currentLongestCount++;
+			}
+			else {//if it stops ascending, store the longest count in array and reset counter
+				longestCounts[i] = currentLongestCount;
+				longestCountIndex++;
+				currentLongestCount = 0;
+			}
+		}
+		int longestCount = 0;
+		//search through array to find longest sequence
+		for(int i = 0; i < longestCounts.length; i++) {
+			if(longestCounts[i] > longestCount) {
+				longestCount = longestCounts[i];
+			}
+		}
+		//return the longest sequence
+		return (longestCount+1);
+	}
+	/**
+	 * Checks to see if elements in array two are in array one
+	 * <p>
+	 * @param array1
+	 * @param array2
+	 * @return true if the elements in array 2 are in array 1, false otherwise
+	 */
+	public static boolean contains(int[] array1, int[] array2) {
+		//Sort arrays
+		boolean contains = true;
+		Arrays.sort(array1);
+		Arrays.sort(array2);
+		int containCount = 0;
+		
+		for(int i = 0; i < array2.length; i++) {//Iterate through each element in array2
+			for(int j = 0;  j < array1.length; j++) {
+				if(array2[i] == array1[j]) {//See if current element in array 2 exists in array one
+					containCount++;
+					break;
+				}
+			}
+		}
+		//Check if number of similar elements match length of array 2
+		if(containCount == array2.length) {
+			contains = true;
+		}else {
+			contains = false;
+		}
+		return contains;
+	}
+	/**
+	 * Collapses an array such that every pair of elements in the array are summed and stored in a new array
+	 * <p>
+	 * @param array
+	 * @return a new int array with the sum of each pair
+	 */
+	public static int[] collapse(int[] array) {
+		int[] collapsedArray;
+		if(array.length % 2 == 0) {//If even, create array that's half the size of the original
+			collapsedArray = new int[array.length/2];
+			int collapseCount = 0;
+			for(int i = 0; i < array.length-1; i+=2) {//iterate through each pair of elements
+				collapsedArray[collapseCount] = array[i] + array[i+1]; //collapse the array
+				collapseCount++;
+			}
+		}else {//If odd, create array half the size of the original plus 1
+			collapsedArray = new int[array.length/2 + 1];
+			int collapseCount = 0;
+			for(int i = 0; i < array.length-1; i+=2) {//iterate through each pair of elements
+				collapsedArray[collapseCount] = array[i] + array[i+1];//collapse the array
+				collapseCount++;
+			}
+			collapsedArray[collapseCount] = array[array.length-1];//add the last element
+		}
+		//return the new, collapsed array
+		return collapsedArray;
+	}
+	/**
+	 * This methods appends one list onto another. List2 will be added to the end of list 1
+	 * <p>
+	 * @param list1
+	 * @param list2
+	 * @return An integer array, which combines the elements of list1 and list2
+	 */
+	public static int[] append(int[] list1, int[] list2) {
+		//create new array with size to hold elements of array1 and array2
+		int[] combinedArray = new int[list1.length + list2.length];
+		//add elements of first array
+		for(int i = 0; i < list1.length; i++) {
+			combinedArray[i] = list1[i];
+		}
+		int startingIndex = list1.length;
+		//add elements of second array
+		for(int i = 0; i < list2.length; i++) {
+			combinedArray[startingIndex] = list2[i];
+			startingIndex++;
+		}
+		//return new array
+		return combinedArray;
+	}
+	/**
+	 * Finds the number of times each vowel occurs in a string
+	 * @param str
+	 * @return an integer array, containing the number of each vowel [a, e, i, o, u]
+	 */
+	public static int[] vowelCount(String str) {
+		//Initialize counters
+		int aCount = 0, eCount = 0, iCount = 0, oCount = 0, uCount = 0;
+		str = str.toLowerCase();
+		//Iterate through each character of the string
+		for(int i = 0; i < str.length(); i++) {
+			char currentChar = str.charAt(i);
+			//increment counters for each vowel that matches
+			if(currentChar == 'a') {
+				aCount++;
+			}else if(currentChar == 'e') {
+				eCount++;
+			}else if(currentChar == 'i') {
+				iCount++;
+			}else if(currentChar == 'o') {
+				oCount++;
+			}else if(currentChar == 'u') {
+				uCount++;
+			}
+		} 
+		//create new array with list of vowels
+		int[] vowelCount = {aCount, eCount, iCount, oCount, uCount};
+		//return array
+		return vowelCount;
+		
+	}
 }
--- APCS Problem Set 08/src/net/mrpaul/apcs/pb050/ps08/ArraysPotpourriTest.java
+++ APCS Problem Set 08/src/net/mrpaul/apcs/pb050/ps08/ArraysPotpourriTest.java
@@ -5,13 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+/**
+ * JUnit Test class for ArraysPotpourri.java. Tests each of the methods that are in ArraysPotpourri.java
+ * @author Sarthak Bhatnagar
+ *
+ */
 public class ArraysPotpourriTest {
 
 	int[] array1 = new int[7];
 	int[] array2 = new int[6];
 	int[] array3 = new int[6];
 	int[] array4 = new int[4];
+	int[] array5 = new int[1];
+	int[] array6 = new int[5];
 	
 	@Before
 	public void setUp() throws Exception {
@@ -41,6 +47,14 @@ public class ArraysPotpourriTest {
 		array4[1] = 10;
 		array4[2] = 12;
 		array4[3] = 17;
+		
+		array5[0] = 1;
+		
+		array6[0] = 5;
+		array6[1] = 5;
+		array6[2] = 6;
+		array6[3] = 7;
+		array6[4] = 9;
 	}
 
 	@After
@@ -48,6 +62,9 @@ public class ArraysPotpourriTest {
 		array1 = null;
 		array2 = null;
 		array3 = null;
+		array4 = null;
+		array5 = null;
+		array6 = null;
 	}
 
 	//Test lastIndexOf method in ArraysPotpouri.java
@@ -82,14 +99,38 @@ public class ArraysPotpourriTest {
 	
 	@Test
 	public void testMode() {
-		
+		assertEquals(ArraysPotpourri.mode(array1), 85);
+		assertEquals(ArraysPotpourri.mode(array6), 5);
 	}
 	
 	@Test
 	public void testStdev() {
-		assertEquals(ArraysPotpourri.stdev(array1), 10.531, 0.05);
+		assertEquals(ArraysPotpourri.stdev(array1), 10.531, 0.2);
 		assertEquals(ArraysPotpourri.stdev(array2), 2.316, 0.2);
-		assertEquals(ArraysPotpourri.stdev(array4), 7.135, 0.1);
+		assertEquals(ArraysPotpourri.stdev(array4), 7.135, 0.2);
+	}
+	
+	@Test
+	public void testKthLargest() {
+		assertEquals(ArraysPotpourri.kthLargest(array1, 2), 99);
+		assertEquals(ArraysPotpourri.kthLargest(array2, 1), 7);
+		assertEquals(ArraysPotpourri.kthLargest(array3, 3), 16);
+		assertEquals(ArraysPotpourri.kthLargest(array4, 2), 10);
+	}
+	
+	@Test
+	public void testMinGap() {
+		assertEquals(ArraysPotpourri.minGap(array4), 2);
+		assertEquals(ArraysPotpourri.minGap(array2), -5);
+		assertEquals(ArraysPotpourri.minGap(array5), 0);
+	}
+	
+	@Test
+	public void testPercentEven() {
+		assertEquals(ArraysPotpourri.percentEven(array5) , 0, 0.001);
+		assertEquals(ArraysPotpourri.percentEven(array2) , 50.0, 0.001);
+		assertEquals(ArraysPotpourri.percentEven(array4) , 75.0, 0.001);
+		assertEquals(ArraysPotpourri.percentEven(array3) , 50.0, 0.001);
 	}
 	
 	@Test
@@ -98,6 +139,73 @@ public class ArraysPotpourriTest {
 		assertTrue(ArraysPotpourri.isUnique(array2));
 		assertTrue(ArraysPotpourri.isUnique(array3));
 		assertTrue(ArraysPotpourri.isUnique(array4));
+	}
+	
+	@Test
+	public void testPriceIsRight() {
+		assertEquals(ArraysPotpourri.priceIsRight(array2, 5), 5);
+		assertEquals(ArraysPotpourri.priceIsRight(array3, 17), 16);
+	}
+	
+	@Test
+	public void testLongestSortedSequence() {
+		assertEquals(ArraysPotpourri.longestSortedSequence(array1), 3);
+		assertEquals(ArraysPotpourri.longestSortedSequence(array2), 3);
+		assertEquals(ArraysPotpourri.longestSortedSequence(array3), 3);
+		assertEquals(ArraysPotpourri.longestSortedSequence(array4), 4);
+	}
+	
+	@Test
+	public void testContains() {
+		int[] list1 = {1, 6, 2, 1, 4, 1, 2, 1, 8};
+		int[] list2 = {1, 2, 1};
+		int[] list3 = {4, 8, 6};
+		assertTrue(ArraysPotpourri.contains(list1, list2));
+		assertTrue(ArraysPotpourri.contains(list1, list3));
+		list1 = null;
+		list2 = null;
+		list3 = null;
+	}
+	
+	@Test
+	public void testCollapse() {
+		int[] list1 = {1, 6, 2, 1, 4, 1, 2, 1, 8};
+		int[] collapsed1 = {7, 3, 5, 3, 8};
+		int[] list2 = {7, 2, 8, 9, 4, 13, 7, 1, 9, 10};
+		int[] collapsed2 = {9,17,17,8,19};
+		assertArrayEquals(ArraysPotpourri.collapse(list1), collapsed1);
+		assertArrayEquals(ArraysPotpourri.collapse(list2), collapsed2);
+		list1 = null;
+		collapsed1 = null;
+		list2 = null;
+		collapsed2 = null;
+	}
+	
+	@Test
+	public void testAppend() {
+		int[] appendarr1 = {1,2,3};
+		int[] appendarr2 = {4,5,6};
+		int[] combinedArr1 = {1,2,3,4,5,6};
+		int[] combinedArr2 = {4,5,6,1,2,3};
+		assertArrayEquals(ArraysPotpourri.append(appendarr1, appendarr2), combinedArr1);
+		assertArrayEquals(ArraysPotpourri.append(appendarr2, appendarr1), combinedArr2);
+		appendarr1 = null;
+		combinedArr1 = null;
+		appendarr2 = null;
+		combinedArr2 = null;
+	}
+	
+	@Test
+	public void testVowelCount() {
+		int[] vowelArray1 = {1, 1, 1, 1, 1};
+		int[] vowelArray2 = {1, 3, 3, 1, 0};
+		int[] vowelArray3 = {0, 6, 0, 2, 0};
+		assertArrayEquals(ArraysPotpourri.vowelCount("aeiou"), vowelArray1);
+		assertArrayEquals(ArraysPotpourri.vowelCount("i think, therefore i am"), vowelArray2);
+		assertArrayEquals(ArraysPotpourri.vowelCount("hello there everyone"), vowelArray3);
+		vowelArray1 = null;
+		vowelArray2 = null;
+		vowelArray3 = null;
 	}
 	
 
